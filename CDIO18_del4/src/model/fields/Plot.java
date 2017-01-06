17ae2e1855803a8e6a141933e328eed5c92e12aa
s144261 <s144261@DTU-CZC6268C75.win.dtu.dk>
2017-01-06 10:32:34 +0100

    Farvekode system oprettet. Fieldlist static

--- CDIO18_del4/src/model/fields/Plot.java
+++ CDIO18_del4/src/model/fields/Plot.java
@@ -5,21 +5,42 @@ import view.Out;
 
 public class Plot extends Ownabel
 {
-	
+	private int groupNumber;
 	private int rent;
 	private boolean[] house = new boolean[6];
 
-	public Plot(String name, String description, int price, int rent, Out out){
+	public Plot(String name, String description, int price, int rent, Out out, int groupNumber){
 		super(name, description, price, out);
 		this.rent = rent;
+		this.groupNumber = groupNumber;
 	}
 
 	@Override
-	public int getRent(Player p){
+	public int getRent(Player p)
+	{
 		// ignore player
-		return rent;
+		for(int i = 0; i < Fieldlist.getFields().length; i++)	// for-loop der kører alle felter igennem.
+		{
+			if(Fieldlist.getFields()[i] instanceof Plot)		// Sorterer alle felter der IKKE er plot fra. Vi tjekker altså om det specifik felt er et Plot-felt.
+			{													
+																// Vi caster felterne til et Plot, så vi kan tilgå groupNumber, og caster igen så vi kan tilgå Owner af felterne.
+																// Og tjekker først om groupNumber er det samme, altså om de er i samme gruppe. Og derefter om ejeren er forskellig.
+				if(((Plot) Fieldlist.getFields()[i]).getGroupNumber() == this.groupNumber && ((Plot) Fieldlist.getFields()[i]).getOwner() != this.getOwner())	
+				{												
+					return rent;								// Returner normal rent.
+				}
+			}
+		}
+			
+		return 2*rent;											// Returnerer double rent.
+			
 	}
 	
+	
+	public int getGroupNumber(){
+		return groupNumber;
+	}
+
 	public void buildhouse(){
 		for (int i = 0; i < house.length; i++) {
 			if(!(house[i])){
@@ -27,16 +48,16 @@ public class Plot extends Ownabel
 				house[i] = true;
 				break;
 			}
-			
+
 			else{
 				// vil du bygge et hotel?
 			} 
-				
+
 		}
-		
-		
+
+
 	}
-	
+
 }
 
 
