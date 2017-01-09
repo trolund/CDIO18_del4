bb3fce195cc77c305d78b0cb396915e13ec0a4c0
s104745 <s104745@DTU-CZC6268C60.win.dtu.dk>
2017-01-09 15:14:46 +0100

    I gang med kort, ikke færdig

--- CDIO18_del4/src/model/cards/Deck.java
+++ CDIO18_del4/src/model/cards/Deck.java
@@ -3,6 +3,7 @@ package model.cards;
 import java.util.Arrays;
 import java.util.Random;
 
+import view.Language;
 import view.Out;
 
 public class Deck {
@@ -10,13 +11,36 @@ public class Deck {
 	public static int cardCount = 0;
 	public static Card[] cards;
 	public Out out;
+	private String[] des = Language.getCardDescriptions();
 
 	public Deck(Out output){
 		this.out = output;
 	
 		cards = new Card[]{
-			
-			
+			new BalanceCard(des[0],40, out), // ret til med doCard metode, 
+			new MoveCard(des[1],11,true,out),
+			new BalanceCard(des[2],10,out),
+			new MoveCard(des[3],3,false,out),
+			new BalanceCard(des[4],50,out),
+			new BalanceCard(des[5],-15,out),
+			new BalanceCard(des[6],50,out),
+			new BalanceCard(des[6],50,out),
+			new MoveCard(des[7],0, true, out),
+			new MoveCard(des[7],0, true, out),
+			new BalanceCard(des[8],998989,out),// Skal laves for sig selv, "familiefest", modtag 500 fra alle spillere. 
+			new MoveCard(des[9],39,true,out),
+			new MoveCard(des[10],5,true,out),
+			new BalanceCard(des[11],50,out),
+			new BalanceCard(des[12],-10,out),
+			new BalanceCard(des[13],-10,out),
+			new BalanceCard(des[14],998989,out),// Ryk til nærmeste færge. modtag 200 ved passering af start
+			new MoveCard(des[15],11,true,out),
+			new MoveCard(des[15],11,true,out),
+			new BalanceCard(des[16],40, out), // ret til med doCard metode, 
+			new BalanceCard(des[17],50, out),
+			new BalanceCard(des[17],50, out),
+			new BalanceCard(des[18],40, out), // ret til med dobeelt leje ved næste redderi
+			new MoveCard(des[19],0,false,out),
 		};
 		shuffleArray();
 	}
