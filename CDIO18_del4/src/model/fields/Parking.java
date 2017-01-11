cc24f3fca480849ec850862234de762274848183
von Scholten <von Scholten@DESKTOP-OQ05U4Q.eduroam.wireless.dtu.dk>
2017-01-11 12:05:59 +0100

    kommentarer til Parking

--- CDIO18_del4/src/model/fields/Parking.java
+++ CDIO18_del4/src/model/fields/Parking.java
@@ -3,24 +3,26 @@ package model.fields;
 import model.Player;
 import view.Out;
 
-public class Parking extends Field
-{
-	
+public class Parking extends Field { //Parking-klassen arver fra Field-klassen
+
 	private int bonus;
 
 	public Parking(String name, String description, int bonus, Out out) 
 	{
-		super(name, description, out);
-		this.bonus = bonus;
+		super(name, description, out); 
+		this.bonus = bonus; //this.bonus er den bonus attributten der blev oprettet i linje 8
 	}
 
 	@Override
-	public void landOn(Player p, Out o)
-	{	
+	public void landOn(Player p, Out o) //skal bruge parametrene Player og Out
+	{
 		p.getAccount().addSum(bonus);
+		// kalder addSum i Player p's account der lægger (bonus) til hens
+		// account
+
 	}
 
-	public int getValue()
+	public int getValue() // get metode returnere bonus
 	{
 		return bonus;
 	}
