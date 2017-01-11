34917bb06497cf5ddab368a69ce09de897408377
bijannegari <bijannegari@bijans-mbp.eduroam.wireless.dtu.dk>
2017-01-11 15:14:54 +0100

    MoveFleetCard undervejs

--- CDIO18_del4/src/model/cards/MoveFleetCard.java
+++ CDIO18_del4/src/model/cards/MoveFleetCard.java
@@ -13,7 +13,7 @@ import view.Out;
 public class MoveFleetCard extends Card {
 
 	private boolean doubleRent;
-	
+
 	public MoveFleetCard(String description, boolean doubleRent, Out out) {
 		super(description, out);
 		this.doubleRent = doubleRent;
@@ -28,7 +28,7 @@ public class MoveFleetCard extends Card {
 			Field f = Fieldlist.getFields()[index];
 			if (f instanceof Fleet) {
 				Fleet fleet = (Fleet)f;
-				
+
 				if ((i + p.getPlayerPos()) > Fieldlist.getFields().length) {
 					p.getAccount().addSum(200);
 				}
@@ -36,9 +36,9 @@ public class MoveFleetCard extends Card {
 				pos = (i + p.getPlayerPos()) % 40;
 
 				if(fleet.getOwner() != null && doubleRent == true){
-					// spilleren skal betale double husleje
+					
 				}
-				
+
 				break;
 			}
 
